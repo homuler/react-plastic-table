3eed5ca78e555458ac8768f6bfbaa10ee536d4e6
homuler <eulerdora.1810.mann@gmail.com>
2020-01-06 21:50:40 +0900

    feat(showcase): add MatrixTable sample

--- packages/react-table-lego-showcase/src/matrix-table.stories.tsx
+++ packages/react-table-lego-showcase/src/matrix-table.stories.tsx
@@ -23,39 +23,7 @@ const StatefulMatrixTable: React.FunctionComponent<Props> = (props: Props) => {
 };
 
 storiesOf('Table/MatrixTable', module)
-  .add('Depth = 1', () => {
-    const colProps = [
-      { style: { width: 200 } },
-      { style: { width: 200 } },
-      { style: { width: 200 } },
-    ];
-    const columns = [
-      { id: 't', name: 'T' },
-      { id: 'f', name: 'F' },
-    ];
-    const rows = [
-      { id: 't', name: 'T' },
-      { id: 'f', name: 'F' },
-    ];
-    const getContent = (columnId: string, rowId: string): string => {
-      if (columnId !== rowId) {
-        return 'F';
-      }
-
-      return columnId === 't' ? 'T' : 'F';
-    };
-
-    return (
-      <StatefulMatrixTable colProps={ colProps } columnLabel='X' rowLabel='Y' columns={ columns } rows={ rows }>
-        {
-          (columnId, rowId, props): React.ReactElement => {
-            return <Td { ...props }>{ getContent(columnId, rowId) }</Td>;
-          }
-        }
-      </StatefulMatrixTable>
-    );
-  })
-  .add('Depth = 2', () => {
+  .add('Truth Table', () => {
     const colProps = [
       { style: { width: 200 } },
       { style: { width: 200 } },
@@ -79,6 +47,72 @@ storiesOf('Table/MatrixTable', module)
 
     return (
       <StatefulMatrixTable colProps={ colProps } columns={ columns } rows={ rows }>
+        {
+          (columnId, rowId, props): React.ReactElement => {
+            return <Td { ...props }>{ getContent(columnId, rowId) }</Td>;
+          }
+        }
+      </StatefulMatrixTable>
+    );
+  })
+  .add('Blood Types Compatibility', () => {
+    const colProps = [
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+      { style: { width: 80 } },
+    ];
+    const columns = [
+      {
+        id: 'donor', name: 'Donor', children: [
+          { id: 'o', name: 'O', children: [{ id: 'o-minus', name: 'O-' }, { id: 'o-plus', name: 'O+' }] },
+          { id: 'a', name: 'A', children: [{ id: 'a-minus', name: 'A-' }, { id: 'a-plus', name: 'A+' }] },
+          { id: 'b', name: 'B', children: [{ id: 'b-minus', name: 'B-' }, { id: 'b-plus', name: 'B+' }] },
+          { id: 'ab', name: 'AB', children: [{ id: 'ab-minus', name: 'AB-' }, { id: 'ab-plus', name: 'AB+' }] },
+        ],
+      },
+    ];
+    const rows = [
+      {
+        id: 'recipient', name: 'Recipient', children: [
+          { id: 'o', name: 'O', children: [{ id: 'o-minus', name: 'O-' }, { id: 'o-plus', name: 'O+' }] },
+          { id: 'a', name: 'A', children: [{ id: 'a-minus', name: 'A-' }, { id: 'a-plus', name: 'A+' }] },
+          { id: 'b', name: 'B', children: [{ id: 'b-minus', name: 'B-' }, { id: 'b-plus', name: 'B+' }] },
+          { id: 'ab', name: 'AB', children: [{ id: 'ab-minus', name: 'AB-' }, { id: 'ab-plus', name: 'AB+' }] },
+        ],
+      },
+    ];
+    const getBloodType = (id: string): [number, number] => {
+      const matched = id.match(/(\w+)-(plus|minus)/) as RegExpMatchArray;
+
+      const group = matched[1] === 'o' ? 0 : matched[1] === 'ab' ? 2 : 1;
+      const rh = matched[2] === 'minus' ? 0 : 1;
+      return [group, rh];
+    };
+    const getContent = (columnId: string, rowId: string): React.ReactElement => {
+      const donorType = getBloodType(columnId);
+      const recipientType = getBloodType(rowId);
+      const yes = <span style={ { color: 'green' } }>✔</span>;
+      const no = <span style={ { color: 'red' } }>✗</span>;
+
+      if (donorType[0] > recipientType[0]) { return no; }
+      if (donorType[0] === 1 && recipientType[0] === 1) {
+        if (columnId[0] != rowId[0]) { return no; } // A x B or B x A
+
+        return donorType[1] > recipientType[1] ? no : yes;
+      }
+      return donorType[1] > recipientType[1] ? no : yes;
+    };
+
+    return (
+      <StatefulMatrixTable colProps={ colProps } columns={ columns } rows={ rows } style={ { textAlign: 'center' } }>
         {
           (columnId, rowId, props): React.ReactElement => {
             return <Td { ...props }>{ getContent(columnId, rowId) }</Td>;
